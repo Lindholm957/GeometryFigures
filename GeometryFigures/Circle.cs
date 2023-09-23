2618d651310854bac5f793a28ff60708e9bf68d4
lindholm <yan957@gmail.com>
2023-09-23 18:34:49 +0300

    added circle

---
+++ GeometryFigures/Circle.cs
@@ -0,0 +1,20 @@
+namespace GeometryFigures
+{
+    public class Circle : IFigure
+    {
+        private double _radius;
+        public double Radius => _radius;
+
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+                throw new ArgumentException("Неправильно указан радиус", nameof(radius));
+            _radius = radius;
+        }
+        
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(_radius, 2);
+        }
+    }
+}
--- GeometryFigures/Program.cs
+++ GeometryFigures/Program.cs
@@ -6,7 +6,9 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var x =new Triangle(1, 2, 2.24);
-        Console.WriteLine(x.IsRightTriangle);
+        var x = new Triangle(1, 2, 2.24);
+        Console.WriteLine(x.GetArea());
+        var y = new Circle(5);
+        Console.WriteLine(y.GetArea());
     }
 }
